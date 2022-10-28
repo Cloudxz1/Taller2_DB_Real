50e4e5ab9d415db5f051cf2560ed60de3e6a0128
Blas_Arias <73445177+BlasArias@users.noreply.github.com>
2022-10-28 04:34:07 -0400

    Consulta punto 2, datos de un vendedor y su edad(años) PD: Revisar

--- Taller2_DB/ConsultarDatosVendedorAnios.Designer.cs
+++ Taller2_DB/ConsultarDatosVendedorAnios.Designer.cs
@@ -30,11 +30,16 @@ namespace Taller2_DB
         private void InitializeComponent()
         {
             this.btnVolDatosVendAnios = new System.Windows.Forms.Button();
+            this.cmbListaVendedor = new System.Windows.Forms.ComboBox();
+            this.ListVenAniosDGV = new System.Windows.Forms.DataGridView();
+            this.btnBusVenAnios = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ListVenAniosDGV)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVolDatosVendAnios
             // 
-            this.btnVolDatosVendAnios.Location = new System.Drawing.Point(22, 404);
+            this.btnVolDatosVendAnios.Location = new System.Drawing.Point(12, 217);
             this.btnVolDatosVendAnios.Name = "btnVolDatosVendAnios";
             this.btnVolDatosVendAnios.Size = new System.Drawing.Size(75, 23);
             this.btnVolDatosVendAnios.TabIndex = 0;
@@ -42,20 +47,66 @@ namespace Taller2_DB
             this.btnVolDatosVendAnios.UseVisualStyleBackColor = true;
             this.btnVolDatosVendAnios.Click += new System.EventHandler(this.btnVolDatosVendAnios_Click);
             // 
+            // cmbListaVendedor
+            // 
+            this.cmbListaVendedor.FormattingEnabled = true;
+            this.cmbListaVendedor.Location = new System.Drawing.Point(27, 97);
+            this.cmbListaVendedor.Name = "cmbListaVendedor";
+            this.cmbListaVendedor.Size = new System.Drawing.Size(143, 21);
+            this.cmbListaVendedor.TabIndex = 1;
+            // 
+            // ListVenAniosDGV
+            // 
+            this.ListVenAniosDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListVenAniosDGV.Location = new System.Drawing.Point(220, 71);
+            this.ListVenAniosDGV.Name = "ListVenAniosDGV";
+            this.ListVenAniosDGV.Size = new System.Drawing.Size(580, 88);
+            this.ListVenAniosDGV.TabIndex = 2;
+            // 
+            // btnBusVenAnios
+            // 
+            this.btnBusVenAnios.Location = new System.Drawing.Point(58, 124);
+            this.btnBusVenAnios.Name = "btnBusVenAnios";
+            this.btnBusVenAnios.Size = new System.Drawing.Size(75, 23);
+            this.btnBusVenAnios.TabIndex = 3;
+            this.btnBusVenAnios.Text = "Buscar";
+            this.btnBusVenAnios.UseVisualStyleBackColor = true;
+            this.btnBusVenAnios.Click += new System.EventHandler(this.btnBusVenAnios_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Buscar Vendedor:";
+            // 
             // ConsultarDatosVendedorAnios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 257);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBusVenAnios);
+            this.Controls.Add(this.ListVenAniosDGV);
+            this.Controls.Add(this.cmbListaVendedor);
             this.Controls.Add(this.btnVolDatosVendAnios);
             this.Name = "ConsultarDatosVendedorAnios";
             this.Text = "ConsultarDatosVendedorAnios";
+            this.Load += new System.EventHandler(this.ConsultarDatosVendedorAnios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ListVenAniosDGV)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnVolDatosVendAnios;
+        private System.Windows.Forms.ComboBox cmbListaVendedor;
+        private System.Windows.Forms.DataGridView ListVenAniosDGV;
+        private System.Windows.Forms.Button btnBusVenAnios;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Taller2_DB/ConsultarDatosVendedorAnios.cs
+++ Taller2_DB/ConsultarDatosVendedorAnios.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1;
+using MySql.Data.MySqlClient;
 
 namespace Taller2_DB
 {
@@ -22,6 +24,33 @@ namespace Taller2_DB
             ConsultasDatos volcons2 = new ConsultasDatos();
             volcons2.Show();
             this.Hide();
+        }
+
+        private void ConsultarDatosVendedorAnios_Load(object sender, EventArgs e)
+        {
+            ConexMySQL conex = new ConexMySQL();
+            conex.open();
+            string query = "SELECT * FROM Vendedor";
+            DataTable t = conex.selectQuery(query);
+
+            for (int i = 0; i < t.Rows.Count; i++)
+            {
+                cmbListaVendedor.Items.Add(t.Rows[i]["NumeroEmpleado"]);
+            }
+            conex.close();
+        }
+        
+        private void btnBusVenAnios_Click(object sender, EventArgs e)
+        {
+            ConexMySQL conex = new ConexMySQL();
+            conex.open();
+            //Consulta con lod datos del vendedor, incluido su atiguedad en anios, con la propiedad DATEDIFF
+            string query = "Select NumeroEmpleado AS Numero, Nombre, Salario, FechaContratacion, DATEDIFF(CURDATE(), FechaContratacion)/365 AS antiguedad From Vendedor WHERE NumeroEmpleado = '" + cmbListaVendedor.Text + "'";
+
+            DataTable t2 = conex.selectQuery(query);
+
+            ListVenAniosDGV.DataSource = t2;
+            ListVenAniosDGV.Show();
         }
     }
 }
