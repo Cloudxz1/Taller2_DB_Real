94551043c05326ef7bf05b3fac187f16c561dbfc
Blas_Arias <73445177+BlasArias@users.noreply.github.com>
2022-10-28 05:02:56 -0400

    Consulta punto 3: Vendedores con mayor y menor antiguedad(años)

--- Taller2_DB/ConsultarDatosVendedorAntigMayorMenor.Designer.cs
+++ Taller2_DB/ConsultarDatosVendedorAntigMayorMenor.Designer.cs
@@ -30,11 +30,18 @@ namespace Taller2_DB
         private void InitializeComponent()
         {
             this.btnVolverVendAntigMayMen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VendedorMayAntiDGV = new System.Windows.Forms.DataGridView();
+            this.VendedorMenAntiDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.VendedorMayAntiDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VendedorMenAntiDGV)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVolverVendAntigMayMen
             // 
-            this.btnVolverVendAntigMayMen.Location = new System.Drawing.Point(29, 404);
+            this.btnVolverVendAntigMayMen.Location = new System.Drawing.Point(29, 390);
             this.btnVolverVendAntigMayMen.Name = "btnVolverVendAntigMayMen";
             this.btnVolverVendAntigMayMen.Size = new System.Drawing.Size(75, 23);
             this.btnVolverVendAntigMayMen.TabIndex = 0;
@@ -42,20 +49,80 @@ namespace Taller2_DB
             this.btnVolverVendAntigMayMen.UseVisualStyleBackColor = true;
             this.btnVolverVendAntigMayMen.Click += new System.EventHandler(this.btnVolverVendAntigMayMen_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vendedor con Mayor antiguedad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vendedor con Menor antiguedad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 31);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Antiguedad del Vendedor";
+            // 
+            // VendedorMayAntiDGV
+            // 
+            this.VendedorMayAntiDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VendedorMayAntiDGV.Location = new System.Drawing.Point(123, 97);
+            this.VendedorMayAntiDGV.Name = "VendedorMayAntiDGV";
+            this.VendedorMayAntiDGV.Size = new System.Drawing.Size(548, 100);
+            this.VendedorMayAntiDGV.TabIndex = 4;
+            this.VendedorMayAntiDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.VendedorMayAntiDGV_CellContentClick);
+            // 
+            // VendedorMenAntiDGV
+            // 
+            this.VendedorMenAntiDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VendedorMenAntiDGV.Location = new System.Drawing.Point(123, 244);
+            this.VendedorMenAntiDGV.Name = "VendedorMenAntiDGV";
+            this.VendedorMenAntiDGV.Size = new System.Drawing.Size(548, 100);
+            this.VendedorMenAntiDGV.TabIndex = 5;
+            this.VendedorMenAntiDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.VendedorMenAntiDGV_CellContentClick);
+            // 
             // ConsultarDatosVendedorAntigMayorMenor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 436);
+            this.Controls.Add(this.VendedorMenAntiDGV);
+            this.Controls.Add(this.VendedorMayAntiDGV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVolverVendAntigMayMen);
             this.Name = "ConsultarDatosVendedorAntigMayorMenor";
             this.Text = "ConsultarDatosVendedorAntigMayorMenor";
+            this.Load += new System.EventHandler(this.ConsultarDatosVendedorAntigMayorMenor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VendedorMayAntiDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VendedorMenAntiDGV)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnVolverVendAntigMayMen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView VendedorMayAntiDGV;
+        private System.Windows.Forms.DataGridView VendedorMenAntiDGV;
     }
 }
--- Taller2_DB/ConsultarDatosVendedorAntigMayorMenor.cs
+++ Taller2_DB/ConsultarDatosVendedorAntigMayorMenor.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1;
+using MySql.Data.MySqlClient;
 
 namespace Taller2_DB
 {
@@ -22,6 +24,48 @@ namespace Taller2_DB
             ConsultasDatos volvenmame = new ConsultasDatos();
             volvenmame.Show();
             this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+ 
+        private void VendedorMenAntiDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+        private void VendedorMayAntiDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        //Muestra a los vendedor menos antiguo en el sistema, minimo 2
+        public void VendedorMenor()
+        {
+            ConexMySQL conex = new ConexMySQL();
+            conex.open();
+            string query = "Select NumeroEmpleado AS Numero, Nombre, Salario, FechaContratacion AS Fecha_Contratacion, DATEDIFF(CURDATE(), FechaContratacion)/365 AS antiguedad From Vendedor Order by FechaContratacion Desc Limit 2;;";
+
+            DataTable t1 = conex.selectQuery(query);
+
+            VendedorMenAntiDGV.DataSource = t1;
+        }
+        //Muestra a los vendedores mas antiguo en el sistema, minimo 2
+        public void VendedorMayor()
+        {
+            ConexMySQL conex = new ConexMySQL();
+            conex.open();
+            string query = "Select NumeroEmpleado AS Numero, Nombre, Salario, FechaContratacion AS Fecha_Contratacion, DATEDIFF(CURDATE(), FechaContratacion)/365 AS antiguedad From Vendedor Order by FechaContratacion Asc Limit 2;;";
+
+            DataTable t2 = conex.selectQuery(query);
+
+            VendedorMayAntiDGV.DataSource = t2;
+        }
+
+        private void ConsultarDatosVendedorAntigMayorMenor_Load(object sender, EventArgs e)
+        {
+            VendedorMayor();
+            VendedorMenor();
         }
     }
 }
