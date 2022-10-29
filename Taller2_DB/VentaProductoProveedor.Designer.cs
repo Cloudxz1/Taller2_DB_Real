779f621223205d3f89a56b9cb1b2db831bae3d57
Blas_Arias <73445177+BlasArias@users.noreply.github.com>
2022-10-29 13:33:39 -0400

    Mod punto 4, proveedor-producto: se volvio a implenetar en un principio, solo mostraba los datagrid. Revisar la cantidad de prod.

--- Taller2_DB/VentaProductoProveedor.Designer.cs
+++ Taller2_DB/VentaProductoProveedor.Designer.cs
@@ -38,10 +38,10 @@ namespace Taller2_DB
             this.label3 = new System.Windows.Forms.Label();
             this.btn_ConfirmarProvProd = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtIdProveedor = new System.Windows.Forms.TextBox();
+            this.txtIdProducto = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtCantidadCompra = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataProd)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataProv)).BeginInit();
@@ -50,9 +50,10 @@ namespace Taller2_DB
             // 
             // btn_Volver
             // 
-            this.btn_Volver.Location = new System.Drawing.Point(12, 12);
+            this.btn_Volver.Location = new System.Drawing.Point(9, 10);
+            this.btn_Volver.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Volver.Name = "btn_Volver";
-            this.btn_Volver.Size = new System.Drawing.Size(93, 39);
+            this.btn_Volver.Size = new System.Drawing.Size(70, 32);
             this.btn_Volver.TabIndex = 0;
             this.btn_Volver.Text = "Volver";
             this.btn_Volver.UseVisualStyleBackColor = true;
@@ -61,65 +62,72 @@ namespace Taller2_DB
             // dataProd
             // 
             this.dataProd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataProd.Location = new System.Drawing.Point(137, 73);
+            this.dataProd.Location = new System.Drawing.Point(103, 59);
+            this.dataProd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataProd.Name = "dataProd";
             this.dataProd.RowHeadersWidth = 51;
             this.dataProd.RowTemplate.Height = 24;
-            this.dataProd.Size = new System.Drawing.Size(470, 222);
+            this.dataProd.Size = new System.Drawing.Size(352, 180);
             this.dataProd.TabIndex = 1;
             // 
             // dataProv
             // 
             this.dataProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataProv.Location = new System.Drawing.Point(706, 73);
+            this.dataProv.Location = new System.Drawing.Point(530, 59);
+            this.dataProv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataProv.Name = "dataProv";
             this.dataProv.RowHeadersWidth = 51;
             this.dataProv.RowTemplate.Height = 24;
-            this.dataProv.Size = new System.Drawing.Size(433, 222);
+            this.dataProv.Size = new System.Drawing.Size(325, 180);
             this.dataProv.TabIndex = 2;
             // 
             // dataProdProv
             // 
             this.dataProdProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataProdProv.Location = new System.Drawing.Point(216, 343);
+            this.dataProdProv.Location = new System.Drawing.Point(162, 279);
+            this.dataProdProv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataProdProv.Name = "dataProdProv";
             this.dataProdProv.RowHeadersWidth = 51;
             this.dataProdProv.RowTemplate.Height = 24;
-            this.dataProdProv.Size = new System.Drawing.Size(313, 106);
+            this.dataProdProv.Size = new System.Drawing.Size(235, 86);
             this.dataProdProv.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(343, 53);
+            this.label1.Location = new System.Drawing.Point(257, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Productos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(903, 53);
+            this.label2.Location = new System.Drawing.Point(677, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Proveedor";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(347, 323);
+            this.label3.Location = new System.Drawing.Point(260, 262);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Venta";
             // 
             // btn_ConfirmarProvProd
             // 
-            this.btn_ConfirmarProvProd.Location = new System.Drawing.Point(906, 407);
+            this.btn_ConfirmarProvProd.Location = new System.Drawing.Point(680, 331);
+            this.btn_ConfirmarProvProd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_ConfirmarProvProd.Name = "btn_ConfirmarProvProd";
-            this.btn_ConfirmarProvProd.Size = new System.Drawing.Size(112, 42);
+            this.btn_ConfirmarProvProd.Size = new System.Drawing.Size(84, 34);
             this.btn_ConfirmarProvProd.TabIndex = 7;
             this.btn_ConfirmarProvProd.Text = "Confirmar";
             this.btn_ConfirmarProvProd.UseVisualStyleBackColor = true;
@@ -128,61 +136,67 @@ namespace Taller2_DB
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(609, 343);
+            this.label4.Location = new System.Drawing.Point(457, 279);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.Size = new System.Drawing.Size(68, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Id Proveedor";
             // 
-            // textBox1
+            // txtIdProveedor
             // 
-            this.textBox1.Location = new System.Drawing.Point(612, 363);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(202, 22);
-            this.textBox1.TabIndex = 9;
+            this.txtIdProveedor.Location = new System.Drawing.Point(459, 295);
+            this.txtIdProveedor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIdProveedor.Name = "txtIdProveedor";
+            this.txtIdProveedor.Size = new System.Drawing.Size(152, 20);
+            this.txtIdProveedor.TabIndex = 9;
             // 
-            // textBox2
+            // txtIdProducto
             // 
-            this.textBox2.Location = new System.Drawing.Point(612, 427);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(202, 22);
-            this.textBox2.TabIndex = 11;
+            this.txtIdProducto.Location = new System.Drawing.Point(459, 347);
+            this.txtIdProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIdProducto.Name = "txtIdProducto";
+            this.txtIdProducto.Size = new System.Drawing.Size(152, 20);
+            this.txtIdProducto.TabIndex = 11;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(609, 407);
+            this.label5.Location = new System.Drawing.Point(457, 331);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 10;
             this.label5.Text = "Id Producto";
             // 
-            // textBox3
+            // txtCantidadCompra
             // 
-            this.textBox3.Location = new System.Drawing.Point(883, 363);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(202, 22);
-            this.textBox3.TabIndex = 13;
+            this.txtCantidadCompra.Location = new System.Drawing.Point(662, 295);
+            this.txtCantidadCompra.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCantidadCompra.Name = "txtCantidadCompra";
+            this.txtCantidadCompra.Size = new System.Drawing.Size(152, 20);
+            this.txtCantidadCompra.TabIndex = 13;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(880, 343);
+            this.label6.Location = new System.Drawing.Point(660, 279);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(132, 17);
+            this.label6.Size = new System.Drawing.Size(99, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "Cantidad a comprar";
             // 
             // VentaProductoProveedor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1211, 474);
-            this.Controls.Add(this.textBox3);
+            this.ClientSize = new System.Drawing.Size(908, 385);
+            this.Controls.Add(this.txtCantidadCompra);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtIdProducto);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtIdProveedor);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btn_ConfirmarProvProd);
             this.Controls.Add(this.label3);
@@ -192,6 +206,7 @@ namespace Taller2_DB
             this.Controls.Add(this.dataProv);
             this.Controls.Add(this.dataProd);
             this.Controls.Add(this.btn_Volver);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "VentaProductoProveedor";
             this.Text = "VentaProductoProveedor";
             this.Load += new System.EventHandler(this.VentaProductoProveedor_Load);
@@ -214,10 +229,10 @@ namespace Taller2_DB
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btn_ConfirmarProvProd;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtIdProveedor;
+        private System.Windows.Forms.TextBox txtIdProducto;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtCantidadCompra;
         private System.Windows.Forms.Label label6;
     }
 }
--- Taller2_DB/VentaProductoProveedor.cs
+++ Taller2_DB/VentaProductoProveedor.cs
@@ -56,7 +56,41 @@ namespace Taller2_DB
 
         private void btn_ConfirmarProvProd_Click(object sender, EventArgs e)
         {
+            
+            ConexMySQL conex = new ConexMySQL();
+            conex.open();
 
+            int cantVender = Int32.Parse(txtCantidadCompra.Text);
+           
+
+                if (cantVender>0)
+                { 
+                    string query0 = "Select Id From Producto Where Id = '" + txtIdProducto.Text + "')";
+                    string idPro = conex.selectQueryScalar(query0);
+                    
+                    string query = "Select CantidadStock From Producto Where Id = '" + txtIdProducto.Text + "')";
+                    string cantStock = conex.selectQueryScalar(query);
+                    int StockA = Int32.Parse(cantStock);
+
+                    int AumentoStock = (StockA + cantVender);
+                    string aumentostock = AumentoStock.ToString();
+
+                    string query2 = "UPDATE Producto SET CantidadStock = ('" + aumentostock + "') WHERE Id = ('" + idPro + "')";
+                    int verificar = conex.executeNonQuery(query2);
+
+                    if (verificar == 1)
+                    {
+                        MessageBox.Show("SI");
+                        string query3 = "INSERT INTO Proveedor_Producto(ProveedorRut, ProductoId) VALUES('" + txtIdProveedor.Text + "','" + txtIdProducto.Text + "')";
+                        int confirmar = conex.executeNonQuery(query3);                      
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La cantidad de productos no puede tener valores negativos");
+                }
+
+            conex.close();
         }
     }
 }
